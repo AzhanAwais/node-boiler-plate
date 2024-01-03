de78df9b249b7192b5a2f4072f72dc9a1409fc04
Azhan Awais <azhan.awais@technado.pk>
2024-01-03 13:44:11 +0500

    working on chat

--- sockets/socketChat.js
+++ sockets/socketChat.js
@@ -1,3 +1,4 @@
+const { getChatUsers } = require("../services/chatService")
 const SocketConnection = require("../sockets/socketConnection")
 
 class SocketChat extends SocketConnection {
@@ -10,18 +11,36 @@ class SocketChat extends SocketConnection {
         const chatIo = this.io.of("/chat")
 
         chatIo.on("connection", async (socket) => {
-            const currUserId = socket?.handshake?.auth?._id
+            const currUser = socket?.handshake?.auth
 
             socket.on("getOnlineUser", async () => {
-                socket.broadcast.emit('getOnlineUser', { userId: currUserId })
+                socket.broadcast.emit('getOnlineUser', { userId: currUser._id })
             })
 
             socket.on("getOfflineUser", async () => {
-                socket.broadcast.emit('getOfflineUser', { userId: currUserId })
+                socket.broadcast.emit('getOfflineUser', { userId: currUser._id })
             })
 
             socket.on("groupCreated", async (data) => {
                 socket.broadcast.emit('groupCreated', { group: data.group })
+            })
+
+            socket.on("joinRoom", async (currUser) => {
+                const chatUsersList = await getChatUsers(currUser)
+                chatUsersList.forEach((item) => {
+                    let roomId = item._id.toString()
+                    socket.join(roomId)
+                })
+            })
+
+            socket.on("message", async (data) => {
+                const roomId = data.chatId.toString()
+                const chatUsersList = await getChatUsers(currUser)
+
+                socket.broadcast.emit('message', {
+                    message: data.messageData,
+                    chatUsersList: chatUsersList
+                })
             })
 
         })
